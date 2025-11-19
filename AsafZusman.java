7c1291f22dc193ea63ef43687ccccbd29234811f
asafzus <asafzusman15@gmail.com>
2025-11-19 16:17:17 +0200

    Updated AsafZusman.java - Different print

--- AsafZusman.java
+++ AsafZusman.java
@@ -1,7 +1,7 @@
 public class AsafZusman{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Asaf Zusman..."+i);
+			System.out.println("Asaf Zusman Modified This Print..."+i);
 		}
 	}
 }
