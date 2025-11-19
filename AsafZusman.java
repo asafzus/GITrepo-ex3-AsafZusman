1f3c4e87aee2499e3845e295b6359ccf04bbe991
asafzus <asafzusman15@gmail.com>
2025-11-19 16:31:35 +0200

    Updated AsafZusman.java - 20 iterations of print

--- AsafZusman.java
+++ AsafZusman.java
@@ -1,6 +1,6 @@
 public class AsafZusman{
 	public static void main(String[] args){
-		for(int i=1;i<=10;i++){
+		for(int i=1;i<=20;i++){
 			System.out.println("Asaf Zusman Modified This Print..."+i);
 		}
 	}
